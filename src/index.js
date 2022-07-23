303dd61dc6133f8a8c4633c4b961b8c47fa52742
AngryTokar <105818110+AngryTokar@users.noreply.github.com>
2022-07-24 00:08:34 +0300

    Правки Хеадер+моб меню

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 // Mobile_menu
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-menu-open]'),
-    closeModalBtn: document.querySelector('[data-menu-close]'),
-    modal: document.querySelector('[data-menu]'),
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
     body: document.querySelector('body'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
-  function toggleModal() {
-    if (refs.modal.classList.contains('is-hidden')) {
-      refs.modal.classList.remove('is-hidden');
-      refs.body.classList.add('no-scroll');
-    } else {
-      refs.modal.classList.add('is-hidden');
-      refs.body.classList.remove('no-scroll');
-    }
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
