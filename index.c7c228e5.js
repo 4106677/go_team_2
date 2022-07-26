2ba4543a2e05b778d49cf273aa363f79d2378ac0
4106677 <104924953+4106677@users.noreply.github.com>
2022-07-26 06:53:01 +0000

    Deploying to gh-pages from @ 4106677/go_team_2@cfadf3cbe2286c8fecc5ff95ef3f32bf0340e911 🚀

---
+++ index.c7c228e5.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("mobile-menu__container-translate"),e.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.c7c228e5.js.map
